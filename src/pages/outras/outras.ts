c04a0cedc64aedcac0db3a8aea85f5bc6d6596e9
Rúben Silva <rubensilva945@gmail.com>
2017-12-03 18:12:00 +0000

    Design das páginas sobre e +moedas concluído

--- src/pages/outras/outras.ts
+++ src/pages/outras/outras.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import { MoedasProvider } from '../../providers/moedas/moedas';
 
 @Component({
-  selector: 'page-outras',
-  templateUrl: 'outras.html'
+  	selector: 'page-outras',
+  	templateUrl: 'outras.html',
+    providers:[MoedasProvider]
 })
 export class OutrasPage {
+	novoOutras: any
+  	constructor(public navCtrl: NavController, private moedasProvider: MoedasProvider, public loadingCtrl: LoadingController) {
+  		this.obterDadosOutras();
+  	}
 
-  constructor(public navCtrl: NavController) {
+  	obterDadosOutras():void{
+  		let loading = this.loadingCtrl.create({
+  			content: 'Obtendo Informações...'
+  		});
 
-  }
+  		loading.present()
+
+  		this.moedasProvider.obterRestantes().subscribe(
+  			outras => {
+  				this.novoOutras = outras
+  			},
+  			err => {
+  				console.error("Erro: " + err);
+  			},
+  			() => {
+  				loading.dismiss();
+  				console.log("Informações obtidas com sucesso");
+  			});
+  	}
 
 }
